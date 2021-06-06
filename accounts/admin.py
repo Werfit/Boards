20a6a1e93e4ff54a2263dace9d427ef860771ce6
werfit <s25112003@gmail.com>
2021-06-06 17:25:08 +0300

    readme updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import *
 
-admin.site.register(User)
-admin.site.register(Reader)
-admin.site.register(Blogger)
+
+class ReaderInline(admin.StackedInline):
+    model = Reader
+    can_delete = False
+    verbose_name_plural = 'ReaderProfile'
+    fk_name = 'user'
+
+
+class BloggerInline(admin.StackedInline):
+    model = Blogger
+    can_delete = False
+    verbose_name_plural = 'BloggerProfile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ReaderInline, BloggerInline)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+
+        if obj.is_blogger:
+            self.inlines = (BloggerInline,)
+        else:
+            self.inlines = (ReaderInline,)
+
+        return super().get_inline_instances(request, obj)
+
+
+admin.site.register(User, CustomUserAdmin)
